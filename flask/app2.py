c2fb42bbbf5ad2d8d58721b3650a4b58430ce936
codeaid-dev <codeaid.lab@gmail.com>
2021-09-29 23:33:57 +0900

    add

---
+++ flask/app2.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('app2.html')
+
+@app.route('/hello')
+def hello():
+    name = request.args.get('name')
+    if name is None:
+        name = '名無し'
+    return render_template('app2_hello.html', name=name)
+
+app.run(port=8000, debug=True)
