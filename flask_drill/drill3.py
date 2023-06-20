0558e6e2d8a4120597aff026b1f3427a7701fd56
codeaid-dev <codeaid.lab@gmail.com>
2023-06-20 13:50:06 +0900

    add

---
+++ flask_drill/drill3.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, url_for, render_template
+
+app = Flask(__name__)
+
+@app.route('/drill3', methods=['GET','POST'])
+def index():
+    seiza = ['山羊座','水瓶座','魚座','牡羊座','牡牛座','双子座','蟹座','獅子座','乙女座','天秤座','蠍座','射手座']
+    date = [19,18,20,19,20,21,22,22,22,23,22,21]
+    if request.method == 'POST' and 'seiza' in request.form:
+        month = int(request.form['month'])
+        day = int(request.form['day'])
+        if month == 2 and day >=29:
+            day = 29
+        elif month in [2,4,6,9,11] and day >= 31:
+            day = 30
+        if date[month-1] >= day:
+            msg = f'{month}月{day}日は{seiza[month-1]}です'
+        else:
+            msg = f'{month}月{day}日は{seiza[month%12]}です'
+    else:
+        msg = None
+    return render_template('drill3.html', msg=msg)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
---
+++ flask_drill/drill4.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, url_for, render_template
+
+app = Flask(__name__)
+
+@app.route('/drill4', methods=['GET','POST'])
+def index():
+    if request.method == 'POST' and 'measure' in request.form:
+        height = float(request.form['height'])
+        weight = float(request.form['weight'])
+        bmi = weight / ((height/100)**2)
+        if bmi < 16:
+            result = '痩せすぎ'
+        elif 16.0 <= bmi <= 16.99:
+            result = '痩せ'
+        elif 17.0 <= bmi <= 18.49:
+            result = '痩せぎみ'
+        elif 18.50 <= bmi <= 24.99:
+            result = '普通体重'
+        elif 25.0 <= bmi <= 29.99:
+            result = '前肥満'
+        elif 30.0 <= bmi <= 34.99:
+            result = '肥満(1度)'
+        elif 35.0 <= bmi <= 39.99:
+            result = '肥満(2度)'
+        else:
+            result = '肥満(3度)'
+        msg = f'BMI値：{bmi:.2f}　判定：{result}です。'
+    else:
+        msg = None
+    return render_template('drill4.html', msg=msg)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
