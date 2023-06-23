25fce59564230cb99a274d92d020568b72c56783
codeaid-dev <codeaid.lab@gmail.com>
2023-06-23 10:09:40 +0900

    add

---
+++ flask_drill/drill8.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, url_for, render_template
+import random
+
+app = Flask(__name__)
+
+def gcd(a, b):
+    if b == 0:
+        return a
+    return gcd(b, a%b)
+
+@app.route('/drill8', methods=['GET','POST'])
+def index():
+    if request.method == 'POST' and 'calc' in request.form:
+        width = int(request.form['width'])
+        height = int(request.form['height'])
+        g = gcd(width, height)
+        rw = width // g
+        rh = height // g
+        msg = f'{rw}:{rh}'
+    else:
+        width = ''
+        height = ''
+        msg = None
+
+    return render_template('drill8.html', msg=msg, width=width, height=height)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
