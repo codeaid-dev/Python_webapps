d7fd1c1d4067b1930142ac8d067cc2378ddfa684
codeaid-dev <codeaid.lab@gmail.com>
2023-07-01 16:57:11 +0900

    add change

--- flask_drill/drill9.py
+++ flask_drill/drill9.py
@@ -1,46 +1,18 @@
-from flask import Flask, request, url_for, render_template
+from flask import Flask, request, url_for, render_template, redirect
 import random
 
 app = Flask(__name__)
 
-cards = [0]*5
-@app.route('/drill9', methods=['GET','POST'])
+@app.route('/drill9')
 def index():
-    if request.method == 'POST':
-        for i in range(5):
-            if f'btn{i+1}' in request.form and cards[i]==0:
-                num = random.randint(1,5)
-                while cards.count(num) >= 4:
-                    num = random.randint(1,5)
-                cards[i] = num
-        print(cards)
-        if not 0 in cards:
-            hand = {}
-            for n in cards:
-                if n not in hand.keys():
-                    hand[n] = cards.count(n)
-
-            val = list(hand.values())
-            if 4 in val:
-                msg = 'フォーカード'
-            elif 2 in val and 3 in val:
-                msg = 'フルハウス'
-            elif 3 in val:
-                msg = 'スリーカード'
-            elif val.count(2) == 2:
-                msg = 'ツーペア'
-            elif val.count(2) == 1:
-                msg = 'ワンペア'
-            else:
-                msg = 'ノーハンド'
-        else:
-            msg = None
+    page = request.args.get('page')
+    print(page)
+    if page:
+        page = int(page)
     else:
-        for i in range(len(cards)):
-            cards[i]=0
-        msg = None
+        return redirect(url_for('index',page=1))
 
-    return render_template('drill9.html', msg=msg, cards=cards)
+    return render_template('drill9.html', page=page)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
