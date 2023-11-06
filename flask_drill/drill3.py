defb96e3b4f7d5494f1be5c5188181a58efb2ebf
codeaid-dev <codeaid.lab@gmail.com>
2023-11-06 14:12:16 +0900

    fix

--- flask_drill/drill3.py
+++ flask_drill/drill3.py
@@ -6,12 +6,12 @@ app = Flask(__name__)
 def index():
     seiza = ['山羊座','水瓶座','魚座','牡羊座','牡牛座','双子座','蟹座','獅子座','乙女座','天秤座','蠍座','射手座']
     date = [19,18,20,19,20,21,22,22,22,23,22,21]
-    if request.method == 'POST' and 'seiza' in request.form:
+    if request.method == 'POST':
         month = int(request.form['month'])
         day = int(request.form['day'])
         if month == 2 and day >=29:
             day = 29
-        elif month in [2,4,6,9,11] and day >= 31:
+        elif month in [4,6,9,11] and day >= 31:
             day = 30
         if date[month-1] >= day:
             msg = f'{month}月{day}日は{seiza[month-1]}です'
