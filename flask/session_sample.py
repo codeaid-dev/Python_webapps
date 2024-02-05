f36ad07d5fea28c9df22f482c1d6a956acf17278
codeaid-dev <codeaid.lab@gmail.com>
2024-02-05 13:17:56 +0900

    fix

--- flask/session_sample.py
+++ flask/session_sample.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, redirect, request, url_for, session
 from datetime import timedelta
+import random, string
 
 app = Flask(__name__)
-app.secret_key = 'Msd4EsJIk6AoVD3g' #セッション情報を暗号化するためのキー
+#app.secret_key = 'Msd4EsJIk6AoVD3g' #セッション情報を暗号化するためのキー
+app.secret_key = ''.join(random.choices(string.ascii_letters + string.digits, k=16)) #セッション情報を暗号化するためのキー
 app.permanent_session_lifetime = timedelta(seconds=30) #セッション有効期限30秒
 
 @app.route('/')
